e319b45d9a2d102b9348bf45aee2babab4c67632
Ryan Dens <ryandens735@gmail.com>
2021-09-25 09:08:07 -0400

    :recycle: move auto project to dependency management and conventions plugins

--- auto/build.gradle.kts
+++ auto/build.gradle.kts
@@ -1,32 +1,14 @@
 plugins {
-    `java-library`
-    id("com.diffplug.spotless")
+    id("com.ryandens.java-library-conventions")
     id("nebula.maven-publish")
 }
 
-group = "com.ryandens"
-version = "0.1.0"
-
-repositories {
-    mavenCentral()
-}
-
-spotless {
-    java {
-        googleJavaFormat()
-    }
-    kotlinGradle {
-        ktlint()
-    }
-}
-
 dependencies {
-    compileOnly("com.google.auto.service", "auto-service-annotations", "1.0")
-    annotationProcessor("com.google.auto.service", "auto-service", "1.0")
+    val autoServiceVersion = "1.0"
+    compileOnly("com.google.auto.service", "auto-service-annotations", autoServiceVersion)
+    annotationProcessor("com.google.auto.service", "auto-service", autoServiceVersion)
     api(project(":lib"))
-    api("org.junit.platform:junit-platform-launcher:1.8.0")
-    implementation(platform("io.opentelemetry:opentelemetry-bom:1.6.0"))
-    implementation(platform("io.opentelemetry:opentelemetry-bom-alpha:1.6.0-alpha"))
+    api("org.junit.platform:junit-platform-launcher")
     api("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
     api("io.opentelemetry:opentelemetry-exporter-otlp-trace")
     api("io.grpc:grpc-netty-shaded:1.39.0")
--- dependencyManagement/build.gradle.kts
+++ dependencyManagement/build.gradle.kts
@@ -10,6 +10,7 @@ javaPlatform {
 dependencies {
     api(enforcedPlatform("org.junit:junit-bom:5.8.1"))
     api(platform("io.opentelemetry:opentelemetry-bom:1.6.0"))
+    api(platform("io.opentelemetry:opentelemetry-bom-alpha:1.6.0-alpha"))
 
     constraints {
         // specifying versions for libraries without BOMs go here
