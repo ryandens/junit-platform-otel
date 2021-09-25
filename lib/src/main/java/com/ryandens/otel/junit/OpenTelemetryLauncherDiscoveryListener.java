414a5ca3b25a1b8f3593605903f48654e54f179b
Ryan Dens <ryandens735@gmail.com>
2021-09-25 09:08:07 -0400

    :sparkles: implement tracing of Launcher + Engine discovery

--- lib/src/main/java/com/ryandens/otel/junit/OpenTelemetryLauncherDiscoveryListener.java
+++ lib/src/main/java/com/ryandens/otel/junit/OpenTelemetryLauncherDiscoveryListener.java
@@ -1,9 +1,13 @@
 package com.ryandens.otel.junit;
 
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.api.trace.TracerProvider;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+import org.junit.platform.engine.DiscoveryFilter;
 import org.junit.platform.engine.DiscoverySelector;
-import org.junit.platform.engine.SelectorResolutionResult;
 import org.junit.platform.engine.UniqueId;
 import org.junit.platform.launcher.EngineDiscoveryResult;
 import org.junit.platform.launcher.LauncherDiscoveryListener;
@@ -19,6 +23,8 @@ public final class OpenTelemetryLauncherDiscoveryListener implements LauncherDis
 
   private final Tracer tracer;
   private final TracerProvider tracerProvider;
+  private final AtomicReference<Span> launcherDiscoverySpan = new AtomicReference<>();
+  private final Map<UniqueId, Span> engineDiscoverySpans = new ConcurrentHashMap<>();
 
   public OpenTelemetryLauncherDiscoveryListener(Tracer tracer, TracerProvider tracerProvider) {
     this.tracer = tracer;
@@ -27,27 +33,42 @@ public final class OpenTelemetryLauncherDiscoveryListener implements LauncherDis
 
   @Override
   public void launcherDiscoveryStarted(LauncherDiscoveryRequest request) {
-    LauncherDiscoveryListener.super.launcherDiscoveryStarted(request);
+    launcherDiscoverySpan.set(
+        tracer
+            .spanBuilder("junit-launcher-discovery")
+            .setAttribute(
+                "junit.discovery.launcher.selectors",
+                request.getSelectorsByType(DiscoverySelector.class).size())
+            .setAttribute(
+                "junit.discovery.launcher.filters",
+                request.getFiltersByType(DiscoveryFilter.class).size())
+            .startSpan());
   }
 
   @Override
   public void launcherDiscoveryFinished(LauncherDiscoveryRequest request) {
-    LauncherDiscoveryListener.super.launcherDiscoveryFinished(request);
+    launcherDiscoverySpan.getAndSet(null).end();
   }
 
   @Override
   public void engineDiscoveryStarted(UniqueId engineId) {
-    LauncherDiscoveryListener.super.engineDiscoveryStarted(engineId);
+    engineDiscoverySpans.put(
+        engineId,
+        tracer
+            .spanBuilder("junit-engine-discovery")
+            .setAttribute("junit.discovery.engine.id", engineId.toString())
+            .startSpan());
   }
 
   @Override
   public void engineDiscoveryFinished(UniqueId engineId, EngineDiscoveryResult result) {
-    LauncherDiscoveryListener.super.engineDiscoveryFinished(engineId, result);
-  }
-
-  @Override
-  public void selectorProcessed(
-      UniqueId engineId, DiscoverySelector selector, SelectorResolutionResult result) {
-    LauncherDiscoveryListener.super.selectorProcessed(engineId, selector, result);
+    final Span span =
+        engineDiscoverySpans
+            .remove(engineId)
+            .setAttribute("junit.discovery.engine.result", result.getStatus().toString());
+    result
+        .getThrowable()
+        .ifPresent(t -> span.setAttribute("junit.discovery.engine.result.message", t.getMessage()));
+    span.end();
   }
 }
