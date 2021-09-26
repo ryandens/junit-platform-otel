912dd15cc8f1cee1367d1dd730613eecd4ef2ca7
Ryan Dens <ryandens735@gmail.com>
2021-09-25 21:19:41 -0400

    :fire: remove publish-verification from root'

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-  id("nebula.publish-verification") apply false
-}
-
 // spotless needs to be able to resolve dependencies on the root project for some reason
 buildscript {
   repositories {
